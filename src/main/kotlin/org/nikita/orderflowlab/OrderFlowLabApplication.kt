cd90b50426adc56e39b8491c5858553e2a261f3f
NAdeKeijzer <n.dk@live.nl>
2026-04-29 09:37:51 +0200

    initial commit

---
+++ src/main/kotlin/org/nikita/orderflowlab/OrderFlowLabApplication.kt
@@ -0,0 +1,11 @@
+package org.nikita.orderflowlab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderFlowLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderFlowLabApplication>(*args)
+}
---
+++ src/test/kotlin/org/nikita/orderflowlab/OrderFlowLabApplicationTests.kt
@@ -0,0 +1,14 @@
+package org.nikita.orderflowlab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class OrderFlowLabApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
