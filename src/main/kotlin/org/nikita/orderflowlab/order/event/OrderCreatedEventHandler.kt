c0b8059dc082c40283f388f771adc227f37ad79a
NAdeKeijzer <n.dk@live.nl>
2026-05-13 17:18:41 +0200

    Extract order created event handler

--- src/main/kotlin/org/nikita/orderflowlab/order/event/OrderCreatedEventHandler.kt
+++ src/main/kotlin/org/nikita/orderflowlab/order/event/OrderCreatedEventHandler.kt
@@ -1,4 +1,19 @@
 package org.nikita.orderflowlab.order.event
 
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
 class OrderCreatedEventHandler {
+
+    private val logger = LoggerFactory.getLogger(OrderCreatedEventHandler::class.java)
+
+    fun handle(event: OrderCreatedEvent) {
+        logger.info(
+            "Received order created event for orderId={}, customerId={}, totalPrice={}",
+            event.orderId,
+            event.customerId,
+            event.totalPrice
+        )
+    }
 }
