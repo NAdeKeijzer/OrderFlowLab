3a9fbdbb7d6ee8c6a0e5127fe5e67449f8d819c0
NAdeKeijzer <n.dk@live.nl>
2026-06-18 11:51:37 +0200

    Schedule outbox event publishing

--- src/main/kotlin/org/nikita/orderflowlab/OrderFlowLabApplication.kt
+++ src/main/kotlin/org/nikita/orderflowlab/OrderFlowLabApplication.kt
@@ -2,7 +2,9 @@ package org.nikita.orderflowlab
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class OrderFlowLabApplication
 
